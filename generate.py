22f061714375c15483876b659fe825f6810862e1
Mark Waite <mark.earl.waite@gmail.com>
2026-02-06 15:31:16 -0700

    Fix string in generator

--- generate.py
+++ generate.py
@@ -14,5 +14,5 @@ with open('verified.json') as json_data:
             my_branch = branch_names[my_id]
         else:
             my_branch = 'master'
-        template = '[![{id}](https://ci.jenkins.io/buildStatus/icon?job=Plugins%2F{id}-plugin%2F{branch})&subject={desc}](https://ci.jenkins.io/job/Plugins/job/{id}-plugin/job/{branch}/)'
+        template = '[![{id}](https://ci.jenkins.io/buildStatus/icon?job=Plugins%2F{id}-plugin%2F{branch}&subject={desc})](https://ci.jenkins.io/job/Plugins/job/{id}-plugin/job/{branch}/)'
         print(template.format(id = my_id, desc = my_desc, branch = my_branch))
