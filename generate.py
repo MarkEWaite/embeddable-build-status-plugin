e72e3cc5aa67df5eb945c2571c4e13d1723e0f79
Mark Waite <mark.earl.waite@gmail.com>
2026-02-06 15:13:19 -0700

    First attempt to extend generate.py

--- generate.py
+++ generate.py
@@ -1,8 +1,11 @@
 import json
+import urllib
 
 with open('verified.json') as json_data:
     d = json.load(json_data)
     for item in d:
         my_id = item[0]
-        my_desc = item[1]
-        print(my_id, my_desc)
+        my_desc = urllib.parse.quote(item[1])
+        my_branch = 'master'
+        template = '[![{id}](https://ci.jenkins.io/buildStatus/icon?job=Plugins%2F{id}-plugin%2F{branch})&subject={desc}](https://ci.jenkins.io/job/Plugins/job/{id}-plugin/job/{branch}/)'
+        print(template.format(id = my_id, desc = my_desc, branch = my_branch))
