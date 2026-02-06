86bab0303fb8f7bea7cd0719f0547bbdca4c89ff
Mark Waite <mark.earl.waite@gmail.com>
2026-02-06 15:12:25 -0700

    Use CBCI 2.541.1.35570

--- plugins-lts.sh
+++ plugins-lts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=2.492.3.5
+VERSION=2.541.1.35570
 ENVELOPE=envelope-core-mm
 [ ! -f temp.json ] && curl -L "https://jenkins-updates.cloudbees.com/update-center/$ENVELOPE/update-center.json?version=$VERSION" | sed -e '1d' -e '$d' > temp.json
 jq '.envelope.plugins | to_entries | [.[]  | select(.value.tier=="verified"  ) | [.key, .value.name, .value.tier]]' temp.json 2>&1 | sed -e 's,Jenkins ,,g' -e 's,Pipeline:,Pipeline,g' -e 's, plugin,,g' -e 's, Plugin,,g' -e 's,Plugin ,,g' | tee verified.json
