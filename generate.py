eadcb3722a7d3fd5cfad3d5effc6f3dcd02392e6
Mark Waite <mark.earl.waite@gmail.com>
2026-02-06 15:40:43 -0700

    Exclude some plugins

--- generate.py
+++ generate.py
@@ -3,13 +3,24 @@ from urllib.parse import quote
 
 branch_names = {}
 branch_names['asm-api'] = 'main'
+branch_names['declarative-pipeline-migration-assistant-api'] = 'main'
 branch_names['json-api'] = 'main'
+branch_names['json-path-api'] = 'main'
+branch_names['saml'] = 'main'
+branch_names['ssh-agents'] = 'main'
+branch_names['token-macro'] = 'main'
+
+exclusions = []
+exclusions.append('declarative-pipeline-migration-assistant-api')
 
 with open('verified.json') as json_data:
     d = json.load(json_data)
+    d.sort()
     for item in d:
         my_id = item[0]
         my_desc = quote(item[1])
+        if my_id in exclusions:
+            continue
         if my_id in branch_names:
             my_branch = branch_names[my_id]
         else:
